055002349c09f3b863d2cdcc97d479f23110dc08
StanisPos <Stanispos@gmail.com>
2018-10-29 15:36:21 +0300

    Added js for modal window

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,2 +1,33 @@
 var link = document.querySelector(".week-product__button-buy");
 var popup = document.querySelector(".modal");
+var background = document.querySelector(".modal__background");
+
+var linkCatalog = document.querySelectorAll(".catalog__icon-price");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  background.classList.add("modal__background--opacity");
+});
+
+background.addEventListener("mouseup", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  background.classList.remove("modal__background--opacity");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt. preventDefault();
+    popup.classList.remove("modal-show");
+    background.classList.remove("modal__background--opacity");
+  }
+});
+
+
+linkCatalog.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  background.classList.add("modal__background--opacity");
+  console.log("Норм?");
+});
