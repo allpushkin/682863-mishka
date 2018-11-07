3ec17c0c42d1736f81af2b9afd3705b1293647a8
StanisPos <Stanispos@gmail.com>
2018-11-07 17:57:00 +0300

    Added styles for icons

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/{icon-*,bg-*}.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
